4e529cff269ddbbe3a5b0613ef3c1ecd6537ca72
unknown <mennameligy567@gmail.com>
2024-01-22 23:59:11 +0100

    building pro modal UI

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,16 +1,71 @@
 "use client";
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { useProModal } from "@/hooks/use-pro-modal";
+import { Badge } from "@/components/ui/badge";
+import { Code, ImageIcon, MessageSquare , Music, VideoIcon } from 'lucide-react';
+import { Card } from "./ui/card";
+
+
+
+const tools = [
+    {
+      label : "conversation",
+      icon  : MessageSquare,
+      color : " text-violet-500",
+      bgColor : "bg-violar-500/10",
+    },
+    {
+      label : "Music Generation",
+      icon  : Music,
+      color : " text-emerald-500",
+      bgColor : "bg-emerald-500/10",
+    },
+    {
+      label : "Image Generation",
+      icon  : ImageIcon,
+      color : " text-pink-700",
+      bgColor : "bg-pink-700/10",
+    },
+    {
+      label : "Video Generation",
+      icon  : VideoIcon,
+      color : " text-orange-700",
+      bgColor : "bg-orange-700/10",
+    },
+    {
+      label : "Image Generation",
+      icon  : Code,
+      color : " text-green-700",
+      bgColor : "bg-green-700/10",
+    },
+    ]
 
 export const ProModal  = () => {
+    const proModel = useProModal();
+
     return (
         <div>
-           <Dialog open>
+           <Dialog open={proModel.isOpen} onOpenChange={proModel.onClose}>
                 <DialogContent>
                         <DialogHeader>
                             <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
-                                Upgrade WebSync
+                                <div className="flex items-center gap-x-2 font-bold py-1">
+                                Upgrade to WebSync
+
+                                <Badge variant="premium" className="uppercase text-sm py-1">
+                                    Pro
+                                </Badge>
+                                </div>
                             </DialogTitle>
+                            <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
+                                    {/* {tools.map(tool) => (
+                                        <Card key={tools.label}
+                                        >
+
+                                        </Card>
+                                    )} */}
+                            </DialogDescription>
                         </DialogHeader>
                 </DialogContent>
            </Dialog>
