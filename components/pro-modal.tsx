fce15c52481a19368d640f5bfbe9d6fe000f1597
unknown <mennameligy567@gmail.com>
2024-01-25 21:18:55 +0100

    prisma db bug resolved

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { useProModal } from "@/hooks/use-pro-modal";
 import { Badge } from "@/components/ui/badge";
-import { Code, ImageIcon, MessageSquare , Music, VideoIcon } from 'lucide-react';
+import { Check, Code, ImageIcon, MessageSquare , Music, VideoIcon, Zap } from 'lucide-react';
 import { Card } from "./ui/card";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 
 
@@ -59,14 +61,34 @@ export const ProModal  = () => {
                                 </div>
                             </DialogTitle>
                             <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
-                                    {/* {tools.map(tool) => (
-                                        <Card key={tools.label}
+                                    {tools.map((tool) => (
+                                        <Card key={tool.label}
+                                        className="p-3 border-black/5 flrx items-center justify-between"
                                         >
+                                            <div className="flex items-center gap-x-4">
+                                                    <div className={cn("p-2 w-fit rounded-md" , tool.bgColor)}>
+                                                        <tool.icon className={cn("w-6 h-6" , tool.color)}/>
+                                                    </div>
+                                                    <div className="font-semibold text-sm">
+                                                        {tool.label}
+                                                    </div>
+                                            </div>
 
+                                            <Check className="text-primary w-5 h-5"/>
                                         </Card>
-                                    )} */}
+                                   ))}
                             </DialogDescription>
                         </DialogHeader>
+                        <DialogFooter>
+                                    <Button
+                                    size="lg"
+                                    variant="premium"
+                                    className="w-full"
+                                    >
+                                        Upgrade
+                                        <Zap className="w-4 h-4 ml-2 fill-white"/>
+                                    </Button>
+                        </DialogFooter>
                 </DialogContent>
            </Dialog>
         </div>
